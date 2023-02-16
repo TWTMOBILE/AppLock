edb233fcb7d36dc815dd1cb8fab409d0bf9af1eb
Davit Abulashvili <d.abulashvili@domino.com.ge>
2023-02-16 13:29:59 +0400

    Splash screen done

--- app/src/main/java/com/example/applock/MainActivity.java
+++ app/src/main/java/com/example/applock/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.applock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/example/applock/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.example.applock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        Intent intent = new Intent(this, (MainActivity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        }, 3000);
+
+
+    }
+}
