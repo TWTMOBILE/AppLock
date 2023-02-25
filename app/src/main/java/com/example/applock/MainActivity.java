025e18f0d044bdcb721ce6d1aacefddc3726fa7c
Davit Abulashvili <d.abulashvili@domino.com.ge>
2023-02-25 15:01:01 +0400

    Fragments added and working except NotificationFragment and SettingsFragment

--- app/src/main/java/com/example/applock/MainActivity.java
+++ app/src/main/java/com/example/applock/MainActivity.java
@@ -5,18 +5,21 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button allAppsBtn, lockedAppsBtn;
+    Button allAppsBtn, lockedAppsBtn, homeBtn;
 
 
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         allAppsBtn = findViewById(R.id.allAppsBtn);
         lockedAppsBtn = findViewById(R.id.lockedAppsBtn);
+        homeBtn = findViewById(R.id.homeBtn);
 
         allAppsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 replaceFragment(new LockedAppsFragment());
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new AllAppsFragment());
             }
         });
 
